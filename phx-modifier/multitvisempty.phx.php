2fe24ca3f3c5ad1e59ab65d5cee3d573c4431d17
Thomas Jakobi <thomas.jakobi@partout.info>
2012-05-24 15:25:40 +0300

    Update phx-modifier/multitvisempty.phx.php

--- phx-modifier/multitvisempty.phx.php
+++ phx-modifier/multitvisempty.phx.php
@@ -1,7 +1,7 @@
 <?php
 /*  
  * description: check whether the multiTV is empty
- * usage: [+phx:multitvisempty=`tvname|docid`:then=``:else=``+]
+ * usage: [+phx:multitvisempty=`tvname|docid`:then=`xxx`:else=`yyy`+]
  * tvname = name of the multiTV
  * docid = id of the document with the multiTV. For use i.e. in ditto (defaults to current document)
  */
