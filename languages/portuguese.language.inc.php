87204620f3f7e8b6c15904469861a3b0d876423d
Thomas Jakobi <thomas.jakobi@partout.info>
2012-05-31 00:41:48 +0200

    added: portuguese language

---
+++ languages/portuguese.language.inc.php
@@ -0,0 +1,16 @@
+<?php
+$language['edit'] = 'Contudo inherited do documento pai e/ou apagado. Clicar <a href="#">aqui</a> para editar.';
+$language['clear'] = 'Apagar todos os registos.';
+$language['confirmclear'] = 'Pretende apagar todos os registos?';
+$language['paste'] = 'Paste table data (i.e. HTML, Word, CSV)';
+$language['pastehere'] = 'Paste here:';
+$language['add'] = 'Adicionar';
+$language['remove'] = 'Apagar';
+$language['save'] = 'Save';
+$language['replace'] = 'Replace';
+$language['append'] = 'Append';
+$language['cancel'] = 'Cancel';
+$language['word'] = 'Word/HTML';
+$language['google'] = 'Google Docs';
+$language['csv'] = 'CSV';
+?>
