995555ac2ca93d26b45ea5735b138e1794766d2a
Thomas Jakobi <thomas.jakobi@partout.info>
2012-06-01 11:36:36 +0200

    update: portuguese language

--- languages/portuguese.language.inc.php
+++ languages/portuguese.language.inc.php
@@ -1,15 +1,15 @@
 <?php
-$language['edit'] = 'Contudo inherited do documento pai e/ou apagado. Clicar <a href="#">aqui</a> para editar.';
+$language['edit'] = 'Conteudo herdado do documento pai e/ou apagado. Clicar <a href="#">aqui</a> para editar.';
 $language['clear'] = 'Apagar todos os registos.';
 $language['confirmclear'] = 'Pretende apagar todos os registos?';
-$language['paste'] = 'Paste table data (i.e. HTML, Word, CSV)';
-$language['pastehere'] = 'Paste here:';
+$language['paste'] = 'Colar tabela aqui (i.e. HTML, Word, CSV)';
+$language['pastehere'] = 'Colar aqui:';
 $language['add'] = 'Adicionar';
 $language['remove'] = 'Apagar';
-$language['save'] = 'Save';
-$language['replace'] = 'Replace';
-$language['append'] = 'Append';
-$language['cancel'] = 'Cancel';
+$language['save'] = 'Guardar';
+$language['replace'] = 'Substituir';
+$language['append'] = 'Anexar';
+$language['cancel'] = 'Cancelar';
 $language['word'] = 'Word/HTML';
 $language['google'] = 'Google Docs';
 $language['csv'] = 'CSV';
