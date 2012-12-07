90158150f9ae463a91127458a71bb64b58a98da1
Thomas Jakobi <thomas.jakobi@partout.info>
2012-12-07 23:17:45 +0100

    Some fixes - Updated jqueryui to the latest legacy version (1.8.24) - Moved css and images to css folder - Small css improvements - Fix for timepicker sliders

--- settings/default.setting.inc.php
+++ settings/default.setting.inc.php
@@ -1,11 +1,11 @@
 <?php
 $settings['css'] = array(
-	'[+tvpath+]js/jquery-ui-1.8.18.custom.css',
-	'[+tvpath+]js/multitv.css');
+	'[+tvpath+]css/jquery-ui-1.8.24.custom.css',
+	'[+tvpath+]css/multitv.css');
 $settings['scripts'] = array(
 	'[+tvpath+]js/jquery-1.4.4.min.js',
 	'[+tvpath+]js/jquery-json-2.3.min.js',
-	'[+tvpath+]js/jquery-ui-1.8.18.custom.min.js',
+	'[+tvpath+]js/jquery-ui-1.8.24.custom.min.js',
 	'[+tvpath+]js/jquery-ui-timepicker-addon.js',
 	'[+tvpath+]js/jquery-field-0.9.6.min.js',
 	'[+tvpath+]js/multitv.js');
--- settings/paste.settings.inc.php
+++ settings/paste.settings.inc.php
@@ -1,12 +1,12 @@
 <?php
 $settings['css'] = array(
-	'[+tvpath+]js/jquery-ui-1.8.18.custom.css',
-	'[+tvpath+]js/multitv.css',
-	'[+tvpath+]js/colorbox.css');
+	'[+tvpath+]css/jquery-ui-1.8.24.custom.css',
+	'[+tvpath+]css/multitv.css',
+	'[+tvpath+]css/colorbox.css');
 $settings['scripts'] = array(
 	'[+tvpath+]js/jquery-1.4.4.min.js',
 	'[+tvpath+]js/jquery-json-2.3.min.js',
-	'[+tvpath+]js/jquery-ui-1.8.18.custom.min.js',
+	'[+tvpath+]js/jquery-ui-1.8.24.custom.min.js',
 	'[+tvpath+]js/jquery-ui-timepicker-addon.js',
 	'[+tvpath+]js/jquery-field-0.9.6.min.js',
 	'[+tvpath+]js/jquery-colorbox-1.3.19.3-min.js',
