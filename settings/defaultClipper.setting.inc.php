216a6fe64f43cde86b7624c627f8a1c39630cdf2
Thomas Jakobi <thomas.jakobi@partout.info>
2013-07-12 09:55:01 +0200

    Update to 1.6     - Clipper 1.2 compatibility

--- settings/defaultClipper.setting.inc.php
+++ settings/defaultClipper.setting.inc.php
@@ -4,9 +4,9 @@ $settings['css'] = array(
 	'css/multitv.css'
 );
 $settings['scripts'] = array(
-	array('name'=>'jquery-json', 'path' => 'js/jquery-json-2.3.min.js'),
-	array('name'=>'jquery-ui', 'path' => 'js/jquery-ui-1.8.24.custom.min.js'),
-	array('name'=>'jquery-ui-timepicker-addon', 'path' => 'js/jquery-ui-timepicker-addon.js'),
-	array('name'=>'jquery-field', 'path' => 'js/jquery-field-0.9.6.min.js')
+	array('name' => 'jquery-json', 'path' => 'js/jquery-json-2.3.min.js'),
+	array('name' => 'jquery-ui', 'path' => 'js/jquery-ui-1.8.24.custom.min.js'),
+	array('name' => 'jquery-ui-timepicker-addon', 'path' => 'js/jquery-ui-timepicker-addon.js'),
+	array('name' => 'jquery-field', 'path' => 'js/jquery-field-0.9.6.min.js')
 );
 ?>
