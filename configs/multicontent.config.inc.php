973e14a0922c738940149d6052c553e2f3f55828
Thomas Jakobi <thomas.jakobi@partout.info>
2013-08-07 22:49:53 +0200

    Update multicontent.config.inc.php

--- configs/multicontent.config.inc.php
+++ configs/multicontent.config.inc.php
@@ -30,7 +30,7 @@ $settings['columns'] = array(
 	array(
 		'caption' => 'Content',
 		'fieldname' => 'title1_1',
-		'render' => '[+fieldTab:switch=`onecol:<table><tr><td>[+title1_1:outer=`<b>|</b> – `+][+content1_1:striphtml:character_limit=`75`:outer=`| …`+]</td></tr></table>|twocol:<table><tr><td style="width: 50%">[+title2_1:outer=`<b>|</b> – `+][+content2_1:striphtml:character_limit=`75`:outer=`| …`+]</td><td style="width: 50%">[+title2_2:outer=`<b>|</b> – `+][+content2_2:striphtml:character_limit=`75`:outer=`| …`+]</td></tr></table>|default:`+]'
+		'render' => '[+fieldTab:switch=`onecol:<table><tr><td>[+title1_1:outer=`<b>|</b> – `+][+content1_1:notags:character_limit=`75`:outer=`| …`+]</td></tr></table>|twocol:<table><tr><td style="width: 50%">[+title2_1:outer=`<b>|</b> – `+][+content2_1:notags:character_limit=`75`:outer=`| …`+]</td><td style="width: 50%">[+title2_2:outer=`<b>|</b> – `+][+content2_2:notags:character_limit=`75`:outer=`| …`+]</td></tr></table>|default:`+]'
 	)
 );
 $settings['form'] = array(
