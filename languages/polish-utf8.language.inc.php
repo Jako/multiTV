23702404e4d0a5bfb5fd53d1f6f5c06630f9c1b9
Witek Galecki <super@witek.org>
2013-09-28 14:06:21 +0200

    Added polish translation

---
+++ languages/polish-utf8.language.inc.php
@@ -0,0 +1,40 @@
+<?php
+$language['editSwitch'] = 'Zawartość odziedziczona z dokumentu-rodzica, albo usunięta. Kliknij <a href="#">tu</a>, aby edytować.';
+$language['clear'] = 'Usuń wszystko';
+$language['confirmclear'] = 'Na pewno usunąć wszystko?';
+$language['paste'] = 'Wklej tabelę (np. HTML, Word, CSV)';
+$language['pastehere'] = 'Wklej tu:';
+$language['add'] = 'Dodaj';
+$language['remove'] = 'Usuń';
+$language['edit'] = 'Edytuj';
+$language['save'] = 'Zapisz';
+$language['replace'] = 'Zastąp';
+$language['append'] = 'Dodaj';
+$language['cancel'] = 'Anuluj';
+$language['word'] = 'Word/HTML';
+$language['google'] = 'Google Docs';
+$language['csv'] = 'CSV';
+$language['dataTables'] = '{
+	"sEmptyTable":     "Brak wpisów w tabeli",
+    "sInfo":           "Wyświetlane _START_ do _END_ z _TOTAL_ wpisów",
+    "sInfoEmpty":      "Wyświetlane 0 do 0 z 0 wpisów",
+    "sInfoFiltered":   "(odfiltrowano z _MAX_ wszystkich)",
+    "sInfoPostFix":    "",
+    "sInfoThousands":  ",",
+    "sLengthMenu":     "Pokaż _MENU_ wpisów",
+    "sLoadingRecords": "Ładowanie…",
+    "sProcessing":     "Przetwarzanie…",
+    "sSearch":         "Szukaj:",
+    "sZeroRecords":    "Brak pasujących wpisów",
+    "oPaginate": {
+        "sFirst":    "Początek",
+        "sLast":     "Koniec",
+        "sNext":     "Następne",
+        "sPrevious": "Poprzednie"
+    },
+    "oAria": {
+        "sSortAscending":  ": aktywuj, by posortować kolumnę rosnąco",
+        "sSortDescending": ": aktywuj, by posortować kolumnę malejąco"
+    }
+}';
+?>
