890591e6594a1543fe6572fd150a1faf70fd8cce
Dmi3yy <dmi3yy@gmail.com>
2014-01-24 09:32:43 +0200

    update for 1.0.13

--- assets/tvs/multitv/settings/datatable.setting.inc.php
+++ assets/tvs/multitv/settings/datatable.setting.inc.php
@@ -6,9 +6,9 @@ $settings['css'] = array(
 	'css/contextMenu.css'
 );
 $settings['scripts'] = array(
-	'js/jquery-colorbox-1.3.19.3-min.js',
+	'js/jquery-colorbox-1.4.33.min.js',
 	'js/jquery-dataTables-1.9.4.min.js',
-	'js/jquery-easytabs-3.1.1.min.js',
+	'js/jquery-easytabs-3.2.0.min.js',
 	'js/jquery-dataTables.rowReordering-1.0.0.js',
 	'js/jquery-contextMenu-1.7.js'
 );
