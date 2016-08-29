60855d2cef0dba3c49db194d7186afcd1dc14933
Deesen <dh@fuseit.de>
2016-08-29 08:38:07 +0200

    Check for old mootools / new jquery datepicker https://github.com/modxcms/evolution/issues/720

--- assets/tvs/multitv/js/multitv.js
+++ assets/tvs/multitv/js/multitv.js
@@ -214,12 +214,13 @@
             var _this = this;
 
             // datepicker
+            var timeFormat = (typeof $.fn.datepicker == 'function') ? 'hh:ii' : 'HH:mm:ss'; // Check for old mootools / new jquery datepicker
             $('.mtvDatePicker', el).click(function () {
                 $.extend(datepickerOptions, {
                     changeMonth: true,
                     changeYear: true,
                     dateFormat: 'dd-mm-yy',
-                    timeFormat: 'HH:mm:ss',
+                    timeFormat: timeFormat,
                     showTimepicker: true
                 });
                 var picker = $(this).datetimepicker(datepickerOptions);
@@ -712,7 +713,7 @@
             if (typeof tinyMCE !== 'undefined') {
                 $('.tabEditor', el).each(function () {
                     var editorId = $(this).attr('id');
-                    if(tinyMCE.majorVersion == 4) {
+                    if (tinyMCE.majorVersion == 4) {
                         tinyMCE.execCommand('mceRemoveEditor', false, editorId);
                     } else {
                         tinyMCE.execCommand('mceRemoveControl', false, editorId);
@@ -828,12 +829,13 @@
             var _this = this;
 
             // datepicker
+            var timeFormat = (typeof $.fn.datepicker == 'function') ? 'hh:ii' : 'HH:mm:ss'; // Check for old mootools / new jquery datepicker
             $('.mtvDatePicker', el).click(function () {
                 $.extend(datepickerOptions, {
                     changeMonth: true,
                     changeYear: true,
                     dateFormat: 'dd-mm-yy',
-                    timeFormat: 'HH:mm:ss',
+                    timeFormat: timeFormat,
                     showTimepicker: true
                 });
                 var picker = $(this).datetimepicker(datepickerOptions);
